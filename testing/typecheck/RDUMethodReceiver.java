d0c53083372b5b8aa4ff5e822889a88ed0c9edd2
txiang61 <j.tt.xiang@gmail.com>
2020-03-30 17:30:35 -0400

    added more test on RDU

---
+++ testing/typecheck/RDUMethodReceiver.java
@@ -0,0 +1,34 @@
+import units.qual.*;
+
+public class RDUMethodReceiver {
+
+    public class MeterClass {
+        @RDU Object get(@m MeterClass this) { return null; }  
+        Object set(@m MeterClass this, @RDU Object x) { return null; }
+    }
+
+    public class SecondClass {
+        @RDU Object get(@s SecondClass this) { return null; }
+        Object set(@s SecondClass this, @RDU Object x) { return null; } 
+    }
+
+    void invoke() {
+        MeterClass mc = new MeterClass();
+        @m Object x = mc.get();
+        mc.set(x);
+
+        SecondClass sc = new SecondClass();
+        @s Object y = sc.get();
+        sc.set(y);
+
+        // :: error: (assignment.type.incompatible) 
+        @m Object z = sc.get();
+        // :: error: (argument.type.incompatible)
+        sc.set(z);
+
+        // :: error: (assignment.type.incompatible) 
+        @s Object w = mc.get();
+        // :: error: (argument.type.incompatible)
+        mc.set(w);
+    }
+}
